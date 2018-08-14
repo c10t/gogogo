7109defed2bcd68711e9748576f9681c1aba5d3f
c10t <c10t@fa.ke>
2018-08-14 09:36:07 +0900

    add test

--- chat/trace/tracer_test.go
+++ chat/trace/tracer_test.go
@@ -1,9 +1,19 @@
 package trace
 
 import {
+	"bytes"
 	"testing"
 }
 
 func TestNew(t *testing.T) {
-	t.Error("No Test")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Return fron New should not be nil")
+	} else {
+		tracer.Trace("Hello trace package.")
+		if buf.String() != "Hello trace package.\n" {
+			t.Errorf("Output wrong text: '%s'", buf.String())
+		}
+	}
 }
