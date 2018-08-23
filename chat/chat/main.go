c766e766fd687346da20d87d745ea98a9b1eaf01
c10t <c10t@fa.ke>
2018-08-23 23:43:56 +0900

    TryAvatars

--- chat/chat/main.go
+++ chat/chat/main.go
@@ -18,7 +18,10 @@ import (
 )
 
 // set the active Avatar implementation
-var avatars Avatar = UseFileSystemAvatar
+var avatars Avatar = TryAvatars{
+	UseFileSystemAvatar,
+	UseAuthAvatar,
+	UseGravatar}
 
 type templateHandler struct {
 	once     sync.Once
