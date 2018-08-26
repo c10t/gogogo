e33b297ce594294232a0e99f7b08dca0e00f3c99
c10t <c10t@fa.ke>
2018-08-26 22:01:58 +0900

    twitter stream channel

--- socialpoll/votes/twitter.go
+++ socialpoll/votes/twitter.go
@@ -145,3 +145,28 @@ func readFromTwitter(votes chan<- string) {
 		}
 	}
 }
+
+func startTwitterStream(stopchan <-chan struct{},
+	votes chan<- string) <-chan struct{} {
+	stoppedchan := make(chan struct{}, 1)
+
+	go func() {
+		defer func() {
+			stoppedchan <- struct{}{}
+		}()
+
+		for {
+			select {
+			case <-stopchan:
+				log.Println("stop to read twitter stream...")
+				return
+			default:
+				log.Println("start to read twitter stream...")
+				readFromTwitter(votes)
+				log.Println("... (waiting) ...")
+				time.Sleep(10 * time.Second)
+			}
+		}
+	}()
+	return stoppedchan
+}
