e20a25ecf540fd5aa365fdb47b853ea1556ceb2f
c10t <c10t@fa.ke>
2018-09-06 03:33:30 +0900

    with functions

--- socialpoll/api/main.go
+++ socialpoll/api/main.go
@@ -31,3 +31,21 @@ func withData(d *mgo.Session, f http.HandlerFunc) http.HandlerFunc {
 		f(w, r)
 	}
 }
+
+func withVars(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		OpenVars(r)
+		defer CloseVars(r)
+
+		fn(w, r)
+	}
+}
+
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+
+		fn(w, r)
+	}
+}
