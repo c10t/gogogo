38f572be5a5e09e7fb7911f5ee4ae9c8db87f459
c10t <c10t@fa.ke>
2018-09-08 23:20:25 +0900

    polls with uncompleted test

--- socialpoll/api/polls.go
+++ socialpoll/api/polls.go
@@ -1,5 +1,60 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
 
-func handlePolls(w http.ResponseWriter, r *http.Request) {}
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type poll struct {
+	ID      bson.ObjectId  `bson:"_id" json:"id"`
+	Title   string         `json:"title"`
+	Options []string       `json:"Options"`
+	Results map[string]int `json:"results,omitempty"`
+}
+
+func handlePolls(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		handlePollsGet(w, r)
+		return
+	case "POST":
+		handlePollsPost(w, r)
+		return
+	case "DELETE":
+		handlePollsDelete(w, r)
+		return
+	}
+
+	respondErr(w, r, http.StatusNotFound)
+}
+
+func handlePollsGet(w http.ResponseWriter, r *http.Request) {
+	db := GetVar(r, "db").(*mgo.Database)
+	c := db.C("polls")
+	var q *mgo.Query
+	p := NewPath(r.URL.Path)
+	if p.HasID() {
+		q = c.FindId(bson.ObjectIdHex(p.ID))
+	} else {
+		q = c.Find(nil)
+	}
+
+	var result []*poll
+	if err := q.All(&result); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, err)
+		return
+	}
+
+	respond(w, r, http.StatusOK, &result)
+}
+
+func handlePollsPost(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("NOT IMPLEMENTED"))
+}
+
+func handlePollsDelete(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("NOT IMPLEMENTED"))
+}
