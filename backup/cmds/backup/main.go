c313ea2dadf786835b8fd42d469b57552f341953
c10t <c10t@fa.ke>
2018-09-13 23:46:26 +0900

    list command

--- backup/cmds/backup/main.go
+++ backup/cmds/backup/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/matryer/filedb"
 )
+
+type path struct {
+	Path string
+	Hash string
+}
 
 func main() {
 	var fatalErr error
@@ -35,9 +43,26 @@ func main() {
 	}
 
 	defer db.Close()
-	_, err = db.C("paths")
+	col, err := db.C("paths")
 	if err != nil {
 		fatalErr = err
 		return
+	}
+
+	switch strings.ToLower(args[0]) {
+	case "list":
+		var path path
+		col.ForEach(func(i int, data []byte) bool {
+			err := json.Unmarshal(data, &path)
+			if err != nil {
+				fatalErr = err
+				return true
+			}
+
+			fmt.Printf("= %s\n", path)
+			return false
+		})
+	case "add":
+	case "remove":
 	}
 }
